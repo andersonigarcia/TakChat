3a7cf23b0ffb46dc4621c4d4c2cad787acd05092
Anderson_analista <andersongarcia.analista@gmail.com>
2020-10-02 18:01:19 -0300

    - Refatoração e inclusão do texto de ajuda

--- TakChat/Service/TakHub.cs
+++ TakChat/Service/TakHub.cs
@@ -9,26 +9,22 @@ namespace TakChat.Service
     {
         public const string HubUrl = "/chat";
 
-        [HubMethodName("SendMessageRoom")]
         public async Task SendMessageRoom(MessageDetail message)
         {
             await Clients.Group(message.Recipient.ToLower()).SendAsync("SendMessageRoom", message.UserName, message.Message, message.Recipient, "");
         }
 
-        [HubMethodName("SendUserRegisterAsync")]
         public async Task SendUserRegisterAsync(MessageDetail message)
         {
             await Clients.OthersInGroup(message.Recipient.ToLower()).SendAsync("SendUserRegisterAsync", message.UserName, message.Message, message.Recipient, "");
         }
 
-        [HubMethodName("SendDirectMessage")]
         public async Task SendDirectMessage(MessageDetail message)
         {
             await Clients.User(message.Recipient.ToLower()).SendAsync("SendDirectMessage", message.UserName, message.Message, message.Recipient, "");
         }
 
-        [HubMethodName("SendChangeRoom")]
-        public async Task ChangeRoom(string roomDisconnect, string roomConnect)
+        public async Task SendChangeRoom(string roomDisconnect, string roomConnect)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomDisconnect);
             await Groups.AddToGroupAsync(Context.ConnectionId, roomConnect);
